082985be21f37bacbea9c558cff19bf1c9a38b32
matheusaguiar <joaomatheusaguiar@gmail.com>
2019-01-20 22:04:34 -0200

    adicionado metodos para buscar e atualizar usuario

--- app/controllers/usuario_controller.js
+++ app/controllers/usuario_controller.js
@@ -2,8 +2,8 @@
 
 const model = require('../models/usuario_model');
 
-module.exports.salvarUsuario = function(db, res){
-    model.salvar(db, function(result){
+module.exports.salvarUsuario = function(db, usuario, res){
+    model.salvar(db, usuario, function(result){
         if(!result.error){
             res.status(200).send(result);
         } else {
@@ -12,3 +12,22 @@ module.exports.salvarUsuario = function(db, res){
     });
 }
 
+module.exports.buscarUsuario = function(db, res){
+    model.buscarUsuario(db, function(result){
+        if(!result.error){
+            res.status(200).send(result);
+        } else {
+            res.status(404).send();
+        }
+    });
+}
+
+module.exports.atualizarUsuario = function(db, usuario, res){
+    model.atualizarUsuario(db, usuario, function(result){
+        if(!result.error){
+            res.status(200).send(result);
+        } else {
+            res.status(404).send();
+        }
+    });
+}
